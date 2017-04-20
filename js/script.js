05aa4d800eea51ca65d4cebb16d038ba073ad92e
isaab <mr.abbasovv@gmail.com>
2017-04-20 12:10:43 +0500

    First commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // var ary_img=['../images/header/header_1.jpg','../images/header/header_2.jpg','../images/header/header_3.jpg'];
 
+
+
+
+var pic = document.getElementsByClassName('pic');
+
+
 $(document).ready(function() {
    $('.dot').click(function(){
       var x=$(this).attr('index');
@@ -11,6 +17,48 @@ $(document).ready(function() {
       var img = $(this).data('src');
       console.log(img);
         $(".content_header").css('background-image','url('+img+')');
+        
+       
       
    })
-});
+
+
+   $('.menutab').click(function(){
+       $('.menu').animate({left: '0'}, "slow");
+   })
+   $('.fa-times').click(function(){
+       $('.menu').animate({left: '-400'}, "slow");
+   })
+   
+    $('.add').click(function(){
+       if($('.nameinput').val()=='' && $('.emailinput').val()==''){
+           $('.nameinput').attr('value','The text field is required.').css('color','red');
+           $('.emailinput').attr('value','The email is required.').css('color','red');
+       }else if($('.nameinput').val()==''){
+           $('.nameinput').attr('value','The text field is required.').css('color','red');
+       }else if($('.emailinput').val()==''){
+           $('.emailinput').attr('value','The email is required.').css('color','red');
+       }
+    });
+   
+   $('.dot_second').click(function(){
+      var y=$(this).attr('index');
+      $('.threedots1 div').each(function(){
+          $(this).removeClass('active1')
+      });
+      $(this).addClass('active1');
+      $('.fullsize').animate({left: y*(-100)+'%'});
+   })
+
+
+      $('.dot_second').click(function(){
+      var y=$(this).attr('index');
+      $('.dots_second div').each(function(){
+          $(this).removeClass('active1')
+      });
+      $(this).addClass('active1');
+ 
+      $('.stage_owl').animate({left: y*(-100)+'%'});
+      console.log( $('.stage_owl').position())
+   })
+ });
